1866ae58cf2b725057f8d3553d9e70daabe9941b
Rohit Bavkar <rohitbavkar95@gmail.com>
2022-02-26 01:49:47 +0530

    deployment setup

---
+++ server.js
@@ -0,0 +1,15 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/sas-client'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/sas-client/'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
