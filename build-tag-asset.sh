45c8a1655e0d34c4106fc1b57013d7f11536c16a
Rohit Bavkar <rohitbavkar95@gmail.com>
2022-04-04 01:03:52 +0530

    r

--- build-tag-asset.sh
+++ build-tag-asset.sh
@@ -25,11 +25,11 @@ git push https://$TOKEN@github.com/$REPO.git $BRANCH
 git push https://$TOKEN@github.com/$REPO.git refs/releases/$VERSION
 
 set +e
-git push https://$TOKEN@github.com/$REPO.git :refs/tags/Development
+git push https://$TOKEN@github.com/$REPO.git :refs/releases/Development
 git tag -d Development
 set -e
 git tag -m "[skip-ci] Development tag" Development
-git push https://$TOKEN@github.com/$REPO.git refs/tags/Development
+git push https://$TOKEN@github.com/$REPO.git refs/releases/Development
 
 
 echo "Package build as zip file..."
