d2f1b921be4bd6e1a346d513aea357f5466c9f69
Rohit Bavkar <rohitbavkar95@gmail.com>
2022-04-04 00:23:35 +0530

    r

--- upload-asset.sh
+++ upload-asset.sh
@@ -13,22 +13,22 @@ BUILD_PATH="$(pwd)"
 filename="$MODULE-$tag.zip" #Filename with path
 filename_path="$BUILD_PATH/$MODULE-$tag.zip" #Filename with path
 
-Validate token.
+#Validate token.
 curl -o /dev/null -sH "$AUTH" $GH_REPO || { echo "Error: Invalid repo, token or network issue!";  exit 1; }
 
-Read asset tags.
+#Read asset tags.
 response=$(curl -sH "$AUTH" $GH_TAGS)
 
 echo response;
 
-Get ID of the asset based on given filename.
+#Get ID of the asset based on given filename.
 eval $(echo "$response" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
 [ "$id" ] || { echo "Error: Failed to get release id for tag: $tag"; echo "$response" | awk 'length($0)<100' >&2; exit 1; }
 
-Upload asset
+#Upload asset
 echo "Uploading asset... $localAssetPath" >&2
 
-Construct url
+#Construct url
 GH_ASSET="https://github.build.ge.com/api/uploads/repos/$owner/$repo/releases/$id/assets?name=$(basename $filename)"
 
 curl --data-binary @"$filename_path" -H "Authorization: token $TOKEN" -H "Content-Type: application/octet-stream" $GH_ASSET
