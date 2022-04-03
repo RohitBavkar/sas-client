6cfe9bb9ac15d5523592b3b36889322c2881e12e
Rohit Bavkar <rohitbavkar95@gmail.com>
2022-04-04 01:28:29 +0530

    s

--- build-tag-asset.sh
+++ build-tag-asset.sh
@@ -6,7 +6,7 @@ BUILD_PATH="$(pwd)"
 MODULE="sas-client"
 TOKEN="$(cat git-token.txt | openssl enc -aes-256-cbc -md sha512 -a -d -pbkdf2 -iter 100000 -salt -pass pass:Secret@123#)"
 REPO="RohitBavkar/$MODULE"
-GITHUB="https://github.com/"
+GITHUB="https://api.github.com/"
 
 # checkout branch
 echo "[INFO] Executing build from directory $(pwd)"
